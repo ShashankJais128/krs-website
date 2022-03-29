910a0a078b69bdd97841d563bce56b79c544d35f
Shashank <shashankjaiswal5255@gmail.com>
2022-03-29 22:32:25 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,29 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import Contact from './components/Contact';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import Events from './components/Events';
-import Members from './components/Members';
-import Recruitment from './components/Recruitment';
-import Signin from './components/Signin';
-import Credits from './components/Credits';
+import Contact from './pages/Contact';
+import Navbar from './pages/Navbar';
+import Home from './pages/Home';
+import Events from './pages/Events';
+import Members from './pages/Members';
+import Recruitment from './pages/Recruitment';
+import Signin from './pages/Signin';
+import Credits from './pages/Credits';
+import About from './pages/About';
+import Achievementss from './pages/Achievements'
 function App() {
   return (
     <div className="App">
       <Router>
       <Navbar />
       <Routes>
-        <Route path='/home' element={<Home/>} />
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
         <Route path='/events' element={<Events/>} />
         <Route path='/members' element={<Members/>} />
         <Route path='/recruitment' element={<Recruitment/>} />
         <Route path='/Credits' element={<Credits />} />
-        <Route path='/achievements' element={<Recruitment/>} />
+        <Route path='/achievements' element={<Achievementss />} />
         <Route path='/signin' element={<Signin/>} />
           
       </Routes>
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  const [visible, setVisible] = useState("right-[100%]")
+  console.log(visible);
+  function menuClick(){
+    console.log("clicked");
+    if(visible){
+      setVisible("")
+    }else if(visible === ""){
+      setVisible("right-[100%]")
+    }
+  }
+  return (
+    <div>
+      <nav className="bg-gray-100">
+        <div className="mx-auto px-6">
+          <div className="flex justify-between">
+            <div className="flex space-x-4">
+              {/* logo */}
+              <div>
+                <Link to="/" className="flex items-center py-5 px-2">
+                <img className="w-10 rounded-full ml-4 cursor-pointer" src="" alt="KRS" />
+                </Link>
+              </div>
+              {/* primary nav */}
+              <div className="hidden md:flex items-center space-x-1 text-lg">
+                <Link to="/" className="py-5 px-3 text-gray-700 hover:text-black hover:underline hover:underline-offset-2">Home</Link>
+                <Link to="/about" className="py-5 px-3 text-gray-700 hover:text-black hover:underline hover:underline-offset-2">About</Link>
+                <Link to="/events" className="py-5 px-3 text-gray-700 hover:text-black hover:underline hover:underline-offset-2">Events</Link>
+                <Link to="/members" className="py-5 px-3 text-gray-700 hover:text-black hover:underline hover:underline-offset-2">Members</Link>
+                <Link to="/credits" className="py-5 px-3 text-gray-700 hover:text-black hover:underline hover:underline-offset-2">Credits</Link>
+                <Link to="/recruitment" className="py-5 px-3 text-gray-700 hover:text-black hover:underline hover:underline-offset-2">Recruitment</Link>
+                <Link to="/achievements" className="py-5 px-3 text-gray-700 hover:text-black hover:underline hover:underline-offset-2">Achievements</Link>
+              </div>
+            </div>
+            {/* secondary nav */}
+            <div className="hidden md:flex text-gray-700 items-center space-x-1 text-lg">
+              <Link to="/signup" className="py-2 px-3 hover:text-black border-2 border-black hover:bg-gray-200 rounded transition duration-300">Signup</Link>
+            </div>
+            {/* mobile button goes here */}
+            <div className="md:hidden flex items-center">
+              <button className="mobile-menu-button" onClick={menuClick}>
+                <img className="w-6 cursor-pointer" src="" alt="menu" /> 
+              </button>
+            </div>
+
+          </div>
+        </div>
+        {/* mobile menu */}
+        <div className={`mobile-menu  flex flex-col ${visible} bg-red-200 w-full h-[100vh] items-center absolute   md:hidden`}>
+          <Link to="/" className="block py-2 px-4 my-1 text-sm hover:bg-gray-200">Home</Link><hr className='w-[60%]' />
+          <Link to="/About" className="block py-2 px-4 my-1 text-sm hover:bg-gray-200">About</Link><hr className='w-[60%]' />
+          <Link to="/Events" className="block py-2 px-4 my-1 text-sm hover:bg-gray-200">Events</Link><hr className='w-[60%]' />
+          <Link to="/Members" className="block py-2 px-4 my-1 text-sm hover:bg-gray-200">Members</Link><hr className='w-[60%]' />
+          <Link to="/Credits" className="block py-2 px-4 my-1 text-sm hover:bg-gray-200">Credits</Link><hr className='w-[60%]' />
+          <Link to="/Recruitment" className="block py-2 px-4 my-1 text-sm hover:bg-gray-200">Recruitment</Link><hr className='w-[60%]' />
+          <Link to="/Blog" className="block py-2 px-4 my-1 text-sm hover:bg-gray-200">Signin</Link>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
